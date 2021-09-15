3288b8401c0d63709947cd7c22d2f720cd68c465
Stefan Lukas <etaloof@gmail.com>
2021-09-15 18:41:54 +0200

    Allow setting the image from an in-memory buffer

--- src/tess.rs
+++ src/tess.rs
@@ -3,9 +3,11 @@ use super::*;
 use std::ops::{Deref, DerefMut};
 use std::ffi::CString;
 use std::fmt::{Debug, Formatter};
+use std::convert::TryInto;
 
 pub use leptess::LepTess;
 use leptess::capi;
+use leptess::leptonica::Pix;
 
 pub struct TessApi(LepTess);
 
@@ -39,16 +41,12 @@ impl TessApi {
         Ok(Self(lep_tess))
     }
 
-    pub fn ocr(&mut self, img: impl Into<Vec<u8>>, width: u32, height: u32) -> Result<String, TesserocrError> {
+    pub fn ocr(&mut self, img: impl AsRef<[u8]>, width: u32, height: u32) -> Result<String, TesserocrError> {
         unsafe {
             capi::TessBaseAPISetPageSegMode(self.raw(), capi::TessPageSegMode_PSM_SINGLE_BLOCK);
         }
 
-        let img = img.into();
-        let img = bgr_to_rgb(img);
-        let img = rgb_to_bmp(&img, width, height);
-
-        self.set_image_from_mem(&img)?;
+        self.set_image_from_buffer(width, height, img.as_ref())?;
         Ok(self.get_utf8_text()?)
     }
 
@@ -62,27 +60,93 @@ impl TessApi {
 
         Ok(())
     }
-}
 
-fn bgr_to_rgb(mut image: Vec<u8>) -> Vec<u8> {
-    assert_eq!(image.len() % 3, 0, "bgr image must have an integer number of pixels");
-    for pixel in image.chunks_exact_mut(3) {
-        if let [b, _g, r] = pixel {
-            std::mem::swap(b, r);
-        } else {
-            panic!("chunks should have length of 3 but got length {}", pixel.len());
+    /// Provide an image for Tesseract to recognize.
+    ///
+    /// This method clears all recognition results, and sets the rectangle to the full image, so it
+    /// may be followed immediately by a `[Self::get_utf8_text]`, and it will automatically perform
+    /// recognition.
+    ///
+    /// Unlike `[LepTess::set_image]` and `[LepTess::set_image_from_mem]`, this method takes an
+    /// uncompressed image in RGB, RGBA or grayscale colors. This means in-memory images can be
+    /// directly passed to this function. There is no need to save the image to a temporary file
+    /// (`[LepTess::set_image]`) or compress it to an intermediate format
+    /// (`[LepTess::set_image_from_mem]`).
+    ///
+    /// This function automatically determines the number color channels from the number of bytes of
+    /// the given image. If the number of pixels (as calculated from `width * height`)
+    /// in the image is equal to this number each pixel occupies exactly one byte.
+    /// This implies each pixel has exactly one color channel and
+    /// the input image is treated as a grayscale image. The same applies to three bytes and
+    /// four bytes per pixel with RGB and RGBA images, respectively. Should none of these
+    /// apply to the input image, the functions considers the input image to be invalid and
+    /// will return an error.
+    pub fn set_image_from_buffer(&mut self, width: u32, height: u32, img: &[u8]) -> Result<(), TesserocrError> {
+        let op = |_| "Out of bounds";
+        let width: i32 = width.try_into().map_err(op)?;
+        let height: i32 = height.try_into().map_err(op)?;
+        let img_len: i32 = img.len().try_into().map_err(op)?;
+        let pixel_count = width * height;
+
+        let (img_channel_count, pix_channel_count) = match img_len / pixel_count {
+            3 => (3, 4), // store rgb images in rgba layout because of alignment
+            c @ (1 | 4) => (c, c),
+            _ => return Err("Supplied image buffer has invalid size".into())
+        };
+
+        let pix = unsafe {
+            // SAFETY: We made sure that the arguments are valid:
+            // - width and height must be i32
+            // - bits_per_pixel must be one of 8, 16, 24 or 32
+            // plus, pixCreate checks these constraints
+            let bits_per_pixel = 8 * pix_channel_count;
+            let pix = capi::pixCreate(width, height, bits_per_pixel);
+            if pix.is_null() {
+                return Err("Couldn't create the pix".into())
+            }
+
+            Pix { raw: pix }
+        };
+
+        let pix_data: &mut [u8] = unsafe {
+            // SAFETY: Leptonica allocates the pix data buffer (does the null check)
+            // and fills it with zeros => we can safely create a reference to this memory
+            let data = capi::pixGetData(pix.raw).cast();
+            let len = (pixel_count * pix_channel_count) as usize;
+            std::slice::from_raw_parts_mut(data, len)
+        };
+
+        match img_channel_count {
+            3 => { // rgb
+                for (&img_rgb, pix_rgba) in chunks_exact::<_, 3>(img)
+                    .zip(chunks_exact_mut::<_, 4>(pix_data)) {
+                    let [r, g, b] = img_rgb;
+                    let alpha = 255; // max alpha means fully opaque
+                    *pix_rgba = [r, g, b, alpha];
+                }
+            }
+            1 | 4 => pix_data.copy_from_slice(img),
+            _ => return Err("Supplied image buffer has invalid size".into())
         }
-    }
 
-    image
+        self.as_raw_handle_mut().set_image(&pix);
+
+        Ok(())
+    }
 }
 
-fn rgb_to_bmp(image: &[u8], width: u32, height: u32) -> Vec<u8> {
-    let mut buffer = Vec::with_capacity(image.len());
+fn chunks_exact<T, const N: usize>(iter: &[T]) -> impl Iterator<Item=&[T; N]> {
+    iter.chunks_exact(N)
+        .map(|chunk|
+            chunk.try_into()
+                .unwrap_or_else(|_| panic!("chunks should have length of {}", N))
+        )
+}
 
-    let _ = image::codecs::bmp::BmpEncoder::new(&mut buffer)
-        .encode(image, width, height, image::ColorType::Rgb8)
-        .unwrap();
-
-    buffer
+fn chunks_exact_mut<T, const N: usize>(iter: &mut [T]) -> impl Iterator<Item=&mut [T; N]> {
+    iter.chunks_exact_mut(N)
+        .map(|chunk|
+                 chunk.try_into()
+                     .unwrap_or_else(|_| panic!("chunks should have length of {}", N))
+        )
 }
